f870a4581bfe83eec02e0f7d27e70a9b12899ffa
brunoBrizo <bbrizolara7@gmail.com>
2022-10-05 17:50:10 -0300

    Add a new S3 bucket

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,4 +1,5 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 
@@ -10,5 +11,11 @@ export class HelloCdkStack extends Stack {
       visibilityTimeout: Duration.seconds(300)
     });
 
+    const bucket = new Bucket(this, 'HelloCdkBucket', {
+      autoDeleteObjects: true,
+      versioned: true,
+      removalPolicy: RemovalPolicy.DESTROY
+    });
+    
   }
 }
